09ae9bcdd17f545a8ce770736a582e26b372b7af
Mohammed Ali <amohammedali2005@gmail.com>
2026-06-10 22:05:42 +0530

    Updated environment URL for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api'
+  production: true,
+  apiUrl: 'https://tourism-management-system-9785.onrender.com/api'
 };
